808b004c60552d26ed358eb7b3d91fa9b1a5dddd
Harrison <h.black1@uni.brighton.ac.uk>
2019-05-09 13:38:48 +0100

    upload

--- group/Plague Game/Assets/Script/enemy.cs
+++ group/Plague Game/Assets/Script/enemy.cs
@@ -35,7 +35,7 @@ public class enemy : MonoBehaviour
 
         if (Vector2.Distance(transform.position, player.position) > stop_dist)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.position.x, transform.position.y), speed * Time.deltaTime);
         }
         else if (Vector2.Distance(transform.position, player.position) < stop_dist && (Vector2.Distance(transform.position, player.position) > retreat_dist))
         {
@@ -43,7 +43,7 @@ public class enemy : MonoBehaviour
         }
         else if (Vector2.Distance(transform.position, player.position) < retreat_dist)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.position.x, transform.position.y), -speed * Time.deltaTime);
         }
 
         if (shot_time <= 0)
@@ -55,7 +55,6 @@ public class enemy : MonoBehaviour
         {
             shot_time -= Time.deltaTime;
         }
-
     }
 
     void OnTriggerEnter2D(Collider2D other)
