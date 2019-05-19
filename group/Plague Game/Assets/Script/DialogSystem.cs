f321208251fcffc6686ba5b12d8f1b105971ce85
Harrison <h.black1@uni.brighton.ac.uk>
2019-05-19 13:09:15 +0100

    floor

--- group/Plague Game/Assets/Script/DialogSystem.cs
+++ group/Plague Game/Assets/Script/DialogSystem.cs
@@ -26,6 +26,7 @@ public class DialogSystem : MonoBehaviour
             if(Input.GetKeyUp(KeyCode.F))
             {
                 rat.ShowBox(dialogue);
+                FleaCount.count += 3;
             }
         }
     }
